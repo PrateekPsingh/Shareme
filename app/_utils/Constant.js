e47da70df9451678506d081b3e4e70d477059736
PrateekPsingh <Prateeksingh5116548@gmail.com>
2024-05-16 11:25:40 +0530

    Landing Page done

---
+++ app/_utils/Constant.js
@@ -0,0 +1,3 @@
+export default{
+    desc:'Drag and drop your files directly on cloud and share it with your friend securely with password and send it on your email.'
+}
---
+++ app/page.js
@@ -0,0 +1,12 @@
+import Image from "next/image";
+import Navbar from "./_components/Navbar";
+import Hero from "./_components/Hero";
+
+export default function Home() {
+  return (
+    <div >
+     <Navbar/>
+     <Hero/>
+    </div>
+  )
+}
